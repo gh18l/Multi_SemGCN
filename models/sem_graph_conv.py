c8b958b50d455f1b6c651990522e2d2817335c96
LGH <675157464@qq.com>
2019-07-04 22:56:55 +0800

    .

--- models/sem_graph_conv.py
+++ models/sem_graph_conv.py
@@ -60,7 +60,18 @@ class MutualSemGraphConv(nn.Module):
     Semantic graph convolution layer
     """
 
-    def __init__(self, in_features, out_features, adj, bias=True):
+    def __init__(self, in_features, out_features, adj, adj_mutual, feature_mutual, bias=True):
+        '''
+        :param in_features: dim of input feature
+        :param out_features: dim of output feature
+        :param adj: self-adj matrix
+        :param adj_mutual: mutual-adj matrix
+        :param feature_mutual: temporarily it is (N-1)N * 34 matrix, such as
+        [x11,y11,...,x172,y172
+         x11,y11,...,x173,y173
+        ...]
+        :param bias:
+        '''
         super(MutualSemGraphConv, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -68,10 +79,15 @@ class MutualSemGraphConv(nn.Module):
         self.W = nn.Parameter(torch.zeros(size=(2, in_features, out_features), dtype=torch.float))
         nn.init.xavier_uniform_(self.W.data, gain=1.414)
 
-        self.adj = adj
-        self.m = (self.adj > 0)
-        self.e = nn.Parameter(torch.zeros(1, len(self.m.nonzero()), dtype=torch.float))
-        nn.init.constant_(self.e.data, 1)
+        ### mutual connection
+        self.adj_mutual = adj_mutual
+        self.m_mutual = (self.adj_mutual > 0)
+        self.m_mutual = self.m_mutual.T ## for calculating
+
+        ## 设连接矩阵为xJN, 在这里我们把两个mutual点之间的权重设置为[x11,y11,x21,y21,...,x172,y172]*W，W为可学习的参数。
+        ## 当然，权重也可以是[xa1,ya1, xa2, ya2].
+        self.adj_mutual_W = nn.Parameter(torch.zeros(size=(self.adj.size(0)*2, self.adj.size(0)), dtype=torch.float))
+        nn.init.xavier_uniform_(self.adj_mutual_W.data, gain=1.414)
 
         if bias:
             self.bias = nn.Parameter(torch.zeros(out_features, dtype=torch.float))
@@ -81,12 +97,17 @@ class MutualSemGraphConv(nn.Module):
             self.register_parameter('bias', None)
 
     def forward(self, input):
-        h0 = torch.matmul(input, self.W[0])
-        h1 = torch.matmul(input, self.W[1])
+        h0 = torch.matmul(input[0], self.W[0])
+        h1 = torch.matmul(input[0], self.W[1])
 
-        adj = -9e15 * torch.ones_like(self.adj).to(input.device)
-        adj[self.m] = self.e  ##给连接矩阵的非0值赋参数，即每一个非0值位置都是一个参数
-        adj = F.softmax(adj, dim=1)  ##对这个矩阵softmax一下
+        e = torch.matmul(input[1], self.adj_mutual_W)  ## N(N-1) * 17
+        e = e.reshape([-1]) ##-->这样来
+        adj_mutual = -9e15 * torch.ones_like(self.adj_mutual).to(input.device)
+
+        adj_mutual[self.m_mutual] = e  ## 给连接矩阵的非0值赋参数，即每一个非0值位置都是一个参数，顺序是从上往下来
+        adj_mutual = adj_mutual.t()
+
+        adj = F.softmax(adj, dim=1)  ## 对这个矩阵softmax一下
 
         M = torch.eye(adj.size(0), dtype=torch.float).to(input.device)
 
