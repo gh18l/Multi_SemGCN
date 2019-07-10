8dced53537cdcd38ce25770e355b84e3abe0afeb
LGH <675157464@qq.com>
2019-07-10 20:12:20 +0800

    .

--- multi_gcn.py
+++ multi_gcn.py
@@ -6,12 +6,30 @@ from common.graph_utils import adj_mx_from_skeleton
 from models.sem_gcn import MultiSemGCN
 import torch.backends.cudnn as cudnn
 import torch
+import torch.nn as nn
+import datetime
+import os
+from common.log import Logger, savefig
+from torch.utils.data import DataLoader
+from common.utils import AverageMeter, lr_decay, save_ckpt
+import time
+from progress.bar import Bar
 def main():
     data_path = ""
     keypoints_path = ""
     hid_dim = 128
     num_layers = 4
     non_local = True
+    lr = 1.0e-3
+    epochs = 30
+    _train = True
+    _lr_decay = 100000
+    lr_gamma = 0.96
+    max_norm = True
+    num_workers = 8
+    snapshot = 5
+    batch_size = 64
+    start_epoch = []
     print('==> Loading multi-person dataset...')
     dataset_path = path.join(data_path)
     dataset = CMUPanoDataset(dataset_path)
@@ -24,3 +42,101 @@ def main():
     adj, adj_mutual = adj_mx_from_skeleton(dataset.skeleton()) #ok
     model_pos = MultiSemGCN(adj, adj_mutual, hid_dim, num_layers=num_layers,
                        nodes_group=dataset.skeleton().joints_group() if non_local else None).to(device)  #ok
+    criterion = nn.MSELoss(reduction='mean').to(device)
+    optimizer = torch.optim.Adam(model_pos.parameters(), lr=lr)
+
+    if _train:
+        start_epoch = 0
+        error_best = None
+        glob_step = 0
+        lr_now = lr
+        ckpt_dir_path = path.join('checkpoint',
+                                  datetime.datetime.now().isoformat() + "_l_%04d_hid_%04d_e_%04d_non_local_%d" % (
+                                  num_layers, hid_dim, epochs, non_local))
+
+        if not path.exists(ckpt_dir_path):
+            os.makedirs(ckpt_dir_path)
+            print('=> Making checkpoint dir: {}'.format(ckpt_dir_path))
+
+        logger = Logger(os.path.join(ckpt_dir_path, 'log.txt'))
+        logger.set_names(['epoch', 'lr', 'loss_train', 'error_eval_p1', 'error_eval_p2'])
+
+    poses_train, poses_train_2d, actions_train = fetch(subjects_train, dataset, keypoints, action_filter, stride)
+    train_loader = DataLoader(PoseGenerator(poses_train, poses_train_2d, actions_train), batch_size=batch_size,
+                              shuffle=True, num_workers=num_workers, pin_memory=True)
+
+    poses_valid, poses_valid_2d, actions_valid = fetch(subjects_test, dataset, keypoints, action_filter, stride)
+    valid_loader = DataLoader(PoseGenerator(poses_valid, poses_valid_2d, actions_valid), batch_size=batch_size,
+                              shuffle=False, num_workers=num_workers, pin_memory=True)
+
+    for epoch in range(start_epoch, epochs):
+        print('\nEpoch: %d | LR: %.8f' % (epoch + 1, lr_now))
+
+        # Train for one epoch
+        epoch_loss, lr_now, glob_step = train(train_loader, model_pos, criterion, optimizer, device, lr, lr_now,
+                                              glob_step, _lr_decay, lr_gamma, max_norm=max_norm)
+
+        # Evaluate
+        error_eval_p1, error_eval_p2 = evaluate(valid_loader, model_pos, device)
+
+        # Update log file
+        logger.append([epoch + 1, lr_now, epoch_loss, error_eval_p1, error_eval_p2])
+
+        # Save checkpoint
+        if error_best is None or error_best > error_eval_p1:
+            error_best = error_eval_p1
+            save_ckpt({'epoch': epoch + 1, 'lr': lr_now, 'step': glob_step, 'state_dict': model_pos.state_dict(),
+                       'optimizer': optimizer.state_dict(), 'error': error_eval_p1}, ckpt_dir_path, suffix='best')
+
+        if (epoch + 1) % snapshot == 0:
+            save_ckpt({'epoch': epoch + 1, 'lr': lr_now, 'step': glob_step, 'state_dict': model_pos.state_dict(),
+                       'optimizer': optimizer.state_dict(), 'error': error_eval_p1}, ckpt_dir_path)
+
+    logger.close()
+    logger.plot(['loss_train', 'error_eval_p1'])
+    savefig(path.join(ckpt_dir_path, 'log.eps'))
+
+def train(data_loader, model_pos, criterion, optimizer, device, lr_init, lr_now, step, decay, gamma, max_norm=True):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    epoch_loss_3d_pos = AverageMeter()
+
+    # Switch to train mode
+    torch.set_grad_enabled(True)
+    model_pos.train()
+    end = time.time()
+
+    bar = Bar('Train', max=len(data_loader))
+    for i, (targets_3d, inputs_2d, _) in enumerate(data_loader):
+        # Measure data loading time
+        data_time.update(time.time() - end)
+        num_poses = targets_3d.size(0)
+
+        step += 1
+        if step % decay == 0 or step == 1:
+            lr_now = lr_decay(optimizer, step, lr_init, decay, gamma)
+
+        targets_3d, inputs_2d = targets_3d.to(device), inputs_2d.to(device)
+        outputs_3d = model_pos(inputs_2d)
+
+        optimizer.zero_grad()
+        loss_3d_pos = criterion(outputs_3d, targets_3d)
+        loss_3d_pos.backward()
+        if max_norm:
+            nn.utils.clip_grad_norm_(model_pos.parameters(), max_norm=1)
+        optimizer.step()
+
+        epoch_loss_3d_pos.update(loss_3d_pos.item(), num_poses)
+
+        # Measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        bar.suffix = '({batch}/{size}) Data: {data:.6f}s | Batch: {bt:.3f}s | Total: {ttl:} | ETA: {eta:} ' \
+                     '| Loss: {loss: .4f}' \
+            .format(batch=i + 1, size=len(data_loader), data=data_time.val, bt=batch_time.avg,
+                    ttl=bar.elapsed_td, eta=bar.eta_td, loss=epoch_loss_3d_pos.avg)
+        bar.next()
+
+    bar.finish()
+    return epoch_loss_3d_pos.avg, lr_now, step
